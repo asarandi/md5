2eb78f328fe6786b86b08dcdc835ef3e5c6bcd27
Alexandr SARANDI <sarandi.alex@gmail.com>
2019-07-22 14:57:22 -0700

    rounds

--- compile.sh
+++ compile.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 rm -f md5 md5.o
-nasm -f macho64 md5.asm
-gcc -e _start md5.o -o md5
+
+os=`uname -s`
+
+if [ "$os" == "Linux" ]; then
+    nasm -f elf64 md5.asm
+    ld md5.o -o md5
+fi
+
+if [ "$os" == "Darwin" ]; then
+    nasm -f macho64 md5.asm
+    gcc -e _start md5.o -o md5
+fi
--- md5.asm
+++ md5.asm
@@ -2,32 +2,111 @@ bits 64
 default rel
 global _start
 section .text
+
 _start:
-
-    ; rdi = data
-    ; rsi = size
-
-    mov     rax, 0x67452301
-    mov     rbx, 0xefcdab89
-    mov     rcx, 0x98badcfe
-    mov     rdx, 0x10325476
-
-    mov     rax, 63
-    not     rax
-    mov     rdx, -64
-    mov     rcx, -63
-
-    mov     rax, rsi
-    inc     rax
-    and     rax, -64
-
-
-    mov     rax, 0x2000001
+    lea     rdi, [rel hello]
+    mov     rsi, 11
+    call    _md5
+    mov     rax, 60
     syscall
 
+_md5:                               ; rdi = data
+                                    ; rsi = size
+    push    rsi
+
+    mov     r10, 0x67452301         ; A
+    mov     r11, 0xefcdab89         ; B
+    mov     r12, 0x98badcfe         ; C
+    mov     r13, 0x10325476         ; D
+
+.new_block:
+    xor     rcx, rcx
+.loop:
+    cmp     rcx, 16
+    jae     .round_2
+    mov     rbx, rcx    ; g
+    mov     rax, r11
+    mov     rdx, rax
+    not     rax
+    and     rax, r13
+    and     rdx, r12
+    or      rax, rdx
+    jmp     .funnel
+.round_2:
+    cmp     rcx, 32
+    jae     .round_3
+    mov     rax, 5
+    mul     rcx
+    inc     rax
+    mov     rbx, rax    ; g
+    mov     rax, r13
+    mov     rdx, rax
+    not     rax
+    and     rax, r12
+    and     rdx, r11
+    or      rax, rdx
+    jmp     .funnel
+.round_3:
+    cmp     rcx, 48
+    jae     .round_4
+    mov     rax, 3
+    mul     rcx
+    add     rax, 5
+    mov     rbx, rax    ; g
+    mov     rax, r11
+    xor     rax, r12
+    xor     rax, r13
+    jmp     .funnel
+.round_4:
+    mov     rax, 7
+    mul     rcx
+    mov     rbx, rax    ; g
+    mov     rax, r13
+    not     rax
+    or      rax, r11
+    xor     rax, r12    
+.funnel:
+    add     rax, r10
+    lea     rdx, [rel K]
+    add     eax, dword [rdx + rcx]
+    and     rbx, 15
+    add     eax, dword [rdi + rbx]
+
+    mov     r10, r13
+    mov     r13, r12
+    mov     r12, r11
+    lea     rdx, [rel s]
+    mov     rbx, rcx
+    shr     rbx, 2
+    and     rbx, 12
+    add     rdx, rbx
+    mov     rbx, rcx
+    and     rbx, 3
+    add     rdx, rbx
+    mov     bl, byte [rdx]
+    xchg    rbx, rcx
+    rol     eax, cl
+    xchg    rbx, rcx
+    add     r11, rax
+    inc     rcx
+    test    rcx, 64
+    jz      .loop
+    add     rdi, 64
+    sub     rsi, 64
+    cmp     rsi, 0
+    jle     .done
+
+    jmp     .new_block
+.done:
+    pop     rsi
+    ret
 
 
 section .data
+
+
+hello:
+db "hello world"
 
 s:
 db 7, 12, 17, 22
